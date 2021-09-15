15fd6729e19a9f768fbd409629348b5d9ff03b10
Claudio <caraya2020@alu.uct.cl>
2021-09-15 19:10:58 -0300

    Formulario y PyMongo
    Lo que hice fue solamente agregar un formulario en el index, que debería más adelante recibir las categorías de nuestra base de datos, además, cree un archivo .py llamado "mongodb" que recibe las categorías de nuestra base de datos, faltaría ver la forma de unirlo con el index. Pueden probar correr el código "mongodb.py" y ver si también les muestra por consola el listado con las categorías.

--- integracion/ejemplo1/main.py
+++ integracion/ejemplo1/main.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-
 import form
 
 
@@ -9,13 +8,12 @@ app =Flask(__name__)
 
 @app.route('/', methods = ['GET','POST'])
 
-def index():
+def log():
     coment_form = form.CommentForm(request.form)
     if request.method == 'POST' and coment_form.validate():
         print(coment_form.username.data)
         print(coment_form.email.data)
     return render_template('index.html', form = coment_form)
-
 
 if __name__ == '__main__':
         app.run(debug = True, port=9000)
