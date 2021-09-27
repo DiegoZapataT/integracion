4928515c51eff02c9685b552ddf8253a7059d151
Oscar <ouribe2020@alu.uct.cl>
2021-09-26 22:07:53 -0300

    Obtención títulos de los párrafos de JSON
    Se crea una nueva función en main.py similar a la creada por Claudio, ademas  se crea un nuevo archivo HTML (title.html) que muestra los valores obtenidos.

--- integracion/ejemplo1/main.py
+++ integracion/ejemplo1/main.py
@@ -24,10 +24,23 @@ def index():
 def buscarCategoria():
   categoria=[]
   for x in coleccion.find({},{ "_id": 0, "categoria": 1}):
-
+        
     categoria.append(str((x['categoria'])))
 
   return render_template('category.html', categoria=categoria)
+
+@app.route('/title', methods = ['GET','POST'])
+def buscarTitulo():
+  titulo_parrafo=[]
+  #parrafo=[] FALTA AGREGAR PARRAFO
+  for x in coleccion.find({},{"documentos"}):
+    for y in x["documentos"]:
+      for z in y["desarrollo"]:
+        titulo_parrafo.append(str(z["titulo_parrafo"]))
+        #parrafo.append(str(z["parrafo"]))
+
+  return render_template('title.html', titulo=titulo_parrafo) #parrafo=parrafo
+
 
 if __name__ == '__main__':
         app.run(debug = True, port=9000)
