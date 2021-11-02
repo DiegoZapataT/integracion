4ad94f5a15b519886982860f66d6f06a1004fb7b
Oscar <ouribe2020@alu.uct.cl>
2021-11-02 17:36:03 -0300

    Eliminación de funciónes sobrantes
    Se deja solo la ruta principal que instancia a las funciones de los Tag

--- integracion/ejemplo1/backend.py
+++ integracion/ejemplo1/backend.py
@@ -10,29 +10,6 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 db = myclient['arquetipos']
 coleccion = db['plantillas']
 
-def Desc_Category():
-  desc_categoria=[]
-  for x in coleccion.find({},{"desc_categoria"}):
-    desc_categoria.append(str(x['desc_categoria']))
-  return desc_categoria
-  
-
-def buscarCategoria():
-  categoria=[]
-  for x in coleccion.find({},{ "_id": 0, "categoria": 1}):
-        
-    categoria.append(str((x['categoria'])))
-  return categoria
-  
-
-def buscarTitulo():
-  titulo_parrafo=[]
-  for x in coleccion.find({},{"documentos"}):
-    for y in x["documentos"]:
-      for z in y["desarrollo"]:
-        titulo_parrafo.append(str(z["titulo_parrafo"]))
-  return titulo_parrafo
-
 def tag_documento():
   ID = int(request.form['ID'])
   for data in coleccion.find({"_id": ID}):
@@ -52,40 +29,6 @@ def tag_desarrollo():
         key.append(i) 
   return key 
 
-def buscarParrafo():
-  parrafo=[]
-  for x in coleccion.find({},{"documentos"}):
-    for y in x["documentos"]:
-      for z in y["desarrollo"]:
-        parrafo.append(str(z["parrafo"]))
-
-  return parrafo
-
-def buscarDesarrollo():
-  desarrollo=[]
-  parrafo = buscarParrafo()
-  tituloParafo = buscarTitulo()
-  id_parrafo = buscarId_parrafo()
-  for x in range(len(tituloParafo)):
-    desarrollo.append(str(id_parrafo[x])+'\n'+str(tituloParafo[x])+'\n'+str(parrafo[x]))
-
-  return desarrollo
-
-def buscarId_parrafo():
-  id_parrafo=[]
-  for x in coleccion.find({},{"documentos"}):
-    for y in x["documentos"]:
-      for z in y["desarrollo"]:
-        id_parrafo.append(str(z["id_parrafo"]))
-  return id_parrafo
-
-def buscarFecha():
-  fecha=[]
-  for x in coleccion.find({},{"documentos"}):
-    for y in x["documentos"]:
-      fecha.append(str(y["fecha"]))
-  return fecha
-
 def buscarJson():
   ID = int(request.form['ID'])
   for data in coleccion.find({"_id": ID}):
@@ -93,59 +36,3 @@ def buscarJson():
     for i in data.keys():
       key.append(i) 
   return data,key
-
-def buscaTitulooo():
-  titulo = []
-  for x in coleccion.find({},{"documentos"}):
-    for y in x['documentos']:
-      titulo.append((y['titulo']))
-
-  return titulo
-
-def buscaTitulo():
-  titulo = []
-  id = int(request.form['id'])
-  resp = coleccion.find({"_id":id})
-  for i in resp:
-      i['documentos']
-      for j in i['documentos']:
-          titulo.append((j['titulo']))
-
-  return titulo
-
-def buscaSubtituloooo():
-  subtitulo = []
-  for x in coleccion.find({},{"documentos"}):
-    for y in x['documentos']:
-          subtitulo.append((y['subtitulo']))
-  return subtitulo
-
-def buscaSubtitulo():
-  subtitulo = []
-  id1 = int(request.form['id1'])
-  resp = coleccion.find({"_id":id1})
-  for i in resp:
-      i['documentos']
-      for j in i['documentos']:
-          subtitulo.append((j['subtitulo']))
-  return subtitulo
-
-def Id_Documento():
-  Id_doc=[]
-  for x in coleccion.find({},{"documentos"}):
-    for y in x["documentos"]:
-      Id_doc.append(str(y["id_documento"]))
-  print (Id_doc)    
-  return Id_doc
-
-
-def Document():
-  Document=[]
-  id_documento= Id_Documento()
-  titulo= buscaTitulooo()
-  subtitulo= buscaSubtituloooo()
-  fecha = buscarFecha()
-  desarrollo = buscarDesarrollo()
-  for x in range(len(subtitulo)):
-    Document.append(str(id_documento[x])+'\n'+str(titulo[x])+'\n'+str(subtitulo[x])+'\n'+str(fecha[x])+'\n'+str(subtitulo[x])+'\n'+str(desarrollo[x]))
-  return Document
--- integracion/ejemplo1/main.py
+++ integracion/ejemplo1/main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request
 from flask import render_template
 from backend import *
 
 app =Flask(__name__)
 
-###############  Rutas  ##################
+###############  Ruta Main  ##################
 @app.route('/', methods = ['GET','POST'])
 def index():
   if request.method == 'POST':
@@ -14,63 +14,7 @@ def index():
     return render_template('index.html', data=data,key=key,key_doc=key_doc,key_des=key_des)
   else:
     return render_template('index.html', data={},key={},key_doc={}, key_des={})
-
-
-@app.route('/index', methods = ['GET','POST'])
-def index_r():
-    return render_template('repuestos/index_repuesto.html')
-
-@app.route('/category', methods = ['GET','POST'])
-def buscarCategori():
-  return render_template('repuestos/category.html', categoria=buscarCategoria())
-
-@app.route('/titulo', methods = ['GET','POST'])
-def buscarTitul():
-  return render_template('repuestos/titulo.html', titulo= buscarTitulo())
-
-@app.route('/parrafo', methods = ['GET','POST'])
-def buscarParraf():
-  return render_template('repuestos/parrafo.html', parrafo= buscarParrafo())
-
-@app.route('/desarrollo', methods = ['GET','POST'])
-def buscarDesarroll():
-  return render_template('repuestos/desarrollo.html', desarrollo=buscarDesarrollo())
-
-@app.route('/fecha', methods = ['GET','POST'])
-def buscarFech():
-  return render_template('repuestos/fecha.html', fecha=buscarFecha())
-
-@app.route('/json', methods = ['GET','POST'])
-def buscarJso():
-  return render_template('repuestos/json.html', json=buscarJson())
-
-@app.route('/titu', methods = ['POST'])
-def buscaTitul():
-  return render_template('repuestos/titu.html', titulo= buscaTitulo())
-
-@app.route('/subtitulo', methods = ['POST'])
-def buscaSubtitul():
-  return render_template('repuestos/subtitulo.html', subtitulo=buscaSubtitulo())
-
-
-@app.route('/Desc_cat', methods = ['GET','POST'])
-def Desc_Categor():
-  return render_template('repuestos/desc_category.html', desc_categoria=Desc_Category())
-
-
-@app.route('/Document', methods = ['GET','POST'])
-def Documen():
-  return render_template('repuestos/documentos.html', Document=Document())
-
-     
+    
 if __name__ == '__main__':
         app.run(debug = True, port=9000)
 
-  # if request.method == 'POST':
-  #       id = request.form['ID']
-  #   tag=[]
-  #   for x in coleccion.find({},{"_id": id}):
-  #     print(x)
-  #   return render_template('index.html' , id = id)
-  # else:  
-  #   return render_template('index.html') 
